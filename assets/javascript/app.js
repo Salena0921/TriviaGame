06bf60920237c0de1bf40a565267f7f80882b616
Karissa McKendall <kmckendall@gmail.com>
2017-11-26 18:33:41 -0500

    added the timer to javascript and set it to start after the start button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,60 @@
 //Question Variables//
-var bank  =[ {
-    question:"What is the correct address of the Dursleys?",
-    possibleAnswers:["4 Private Drive", "4 Privet Drive", "14 Prime Drive"], 
-    answer:"4 Privet Drive"
-},
-{question:"Where is the names of Harry's aunt, uncle, and cousin?",
- possibleAnswers:["Patricia,Vernon, Dursely", "Petunia, Vernon, Dudley", "Patty, Victor, Daniel"],
- answer:"Petunia, Vernon, Dudley"}
+var bank = [{
+        question: "What is the correct address of the Dursleys?",
+        possibleAnswers: ["4 Private Drive", "4 Privet Drive", "14 Prime Drive"],
+        answer: "4 Privet Drive"
+    },
+    {
+        question: "Where is the names of Harry's aunt, uncle, and cousin?",
+        possibleAnswers: ["Patricia,Vernon, Dursely", "Petunia, Vernon, Dudley", "Patty, Victor, Daniel"],
+        answer: "Petunia, Vernon, Dudley"
+    }
 ];
 
+var timeClock = false;
+var time = 31
+
+//When page loads//
+$(document).ready(function () {
+    $(".questions").hide();
+    var correctAnswer = 0;
+    var wrongAnswer = 0;
+
+    $("#correct").text(correctAnswer);
+    $("#wrong").text(wrongAnswer);    
+    $("#startb").click(function (){
+        //When Question is displayed//        
+        $(".startpage").hide();
+        $(".questions").show();
+        if(!timeClock){             
+            intervalTime = setInterval(decrement, 1000);
+            timeClock = true;            
+            console.log(timeClock);
+
+        }
+        
+    });
+});
+
 //Start Game//
-//When Question is displayed//
-randomQuestion = bank[Math.floor(Math.random() * bank.length)];
-document.getElementById("question").innerHTML = randomQuestion.question;
-document.getElementById("answer1").innerHTML = randomQuestion.possibleAnswers[0];
-document.getElementById("answer2").innerHTML = randomQuestion.possibleAnswers[1];
-document.getElementById("answer3").innerHTML = randomQuestion.possibleAnswers[2];
-console.log(bank);
-console.log(randomQuestion);
+$(".questions").each(function play() {
+    randomQuestion = bank[Math.floor(Math.random() * bank.length)];
+    $("#question").text(randomQuestion.question);
+    document.getElementById("answer1").innerHTML = randomQuestion.possibleAnswers[0];
+    document.getElementById("answer2").innerHTML = randomQuestion.possibleAnswers[1];
+    document.getElementById("answer3").innerHTML = randomQuestion.possibleAnswers[2];
+    console.log(bank);
+    console.log(randomQuestion);
+    
+});
+
+
+
 //timer//
+function decrement() {
+    time--;
+    $("#time").html(time)
+};
+
 //Is the question worng or right//
 //Logging points//
