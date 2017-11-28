57c00aca9be63a110b4d2f9e8cdb4098ad669290
Karissa McKendall <kmckendall@gmail.com>
2017-11-27 20:09:47 -0500

    update timer to show seconds on the front page, also set up scores to updated according to whether the user selects the right answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ var bank = [{
 ];
 
 var timeClock = false;
-var time = 31
+var time = 16;
 var correctAnswer = 0;
 var wrongAnswer = 0;
 
@@ -34,6 +34,7 @@ $(document).ready(function () {
             console.log(timeClock);
         }
     });
+    
 });
 
 //Start Game//
@@ -43,32 +44,83 @@ $(".questions").each(function play() {
     document.getElementById("answer1").innerHTML = randomQuestion.possibleAnswers[0];
     document.getElementById("answer2").innerHTML = randomQuestion.possibleAnswers[1];
     document.getElementById("answer3").innerHTML = randomQuestion.possibleAnswers[2];
-    // $(".answers").click();  
+    
     console.log(bank);
     console.log(randomQuestion);
-    
+    answer();
+
 });
 
-$(".answers").click(function answer() {    
-    for (var i = 0; i < randomQuestion.possibleAnswers.length; i++) {
-        var userGuess = randomQuestion.possibleAnswers[i];
-        console.log(userGuess)
-        //Is the question worng or right//    
-        if (userGuess === randomQuestion.answer) {
-            //Logging points//
-            correctAnswer++;
-            console.log("yes");
-            $("#correct").text(correctAnswer);
-        } else if (time === 0 || userGuess !== randomQuestion.answer) {
+function answer() {    
+    $("#answer1").click(function () {
+        console.log(randomQuestion.possibleAnswers[0]);
+        if(randomQuestion.possibleAnswers[0] === randomQuestion.answer){
+            console.log("win");
+            correctAnswer++;  
+            $("#correct").text(correctAnswer);        
+        }else{
+            console.log("lose");
             wrongAnswer++;
             $("#wrong").text(wrongAnswer);
-            console.log("yes1");
-            decrement();            
-        };
-    };
-});
+        }
+    });
+    // $("#answer2").click(function () {
+    //     console.log(randomQuestion.possibleAnswers[1]);
+    //     if(randomQuestion.possibleAnswers[1] === randomQuestion.answer){
+    //         console.log("win");
+    //         correctAnswer++;  
+    //         $("#correct").text(correctAnswer);        
+    //     }else{
+    //         console.log("lose");
+    //         wrongAnswer++;
+    //         $("#wrong").text(wrongAnswer);
+    //     }
+    // });
+    // $("#answer3").click(function () {
+    //     console.log(randomQuestion.possibleAnswers[2]);
+    //     if(randomQuestion.possibleAnswers[2] === randomQuestion.answer){
+    //         console.log("win");
+    //         correctAnswer++;  
+    //         $("#correct").text(correctAnswer);        
+    //     }else{
+    //         console.log("lose");
+    //         wrongAnswer++;
+    //         $("#wrong").text(wrongAnswer);
+    //     }
+    // });
+    decrement();
+};
+
+// function answer() {   
+//       $(this).attr("click") = randomQuestion.possibleAnswers;      
+//       console.log("yes");
+// // for (var i = 0; i < randomQuestion.possibleAnswers.length; i++) {
+
+// //     var userGuess = $(".answers").click();
+// //     console.log(userGuess)    
+//     // Is the question wrong or right//    
+//     // if (userGuess === randomQuestion.answer) {
+//     //     //Logging points//
+//     //     console.log(randomQuestion.answer)
+//     //     correctAnswer++;
+//     //     console.log("yes");
+//     //     $("#correct").text(correctAnswer);
+//     // } else{            
+//     //     wrongAnswer++;            
+//     //     $("#wrong").text(wrongAnswer);
+//     //     console.log("yes1");
+//     //     decrement();            
+//     // };
+// };
+
+// };
 //timer//
 function decrement() {
     time--;
     $("#time").html(time)
+    if(time === 0){
+        wrongAnswer++;
+        $("#wrong").text(wrongAnswer);
+    }
+
 };
